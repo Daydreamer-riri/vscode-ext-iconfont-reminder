bf7268894f94c8466e073c85570d83a422bf343a
ding.zhao <ding.zhao@trip.com>
2023-03-08 17:03:44 +0800

    chore: 整理

--- src/extension.ts
+++ src/extension.ts
@@ -7,20 +7,8 @@ import { type Config, resolveConfig } from './config'
 let config: Config | null = null
 
 export function activate(context: ExtensionContext) {
-  console.log(
-    'Congratulations, your extension "iconfont-display" is now active!'
-  )
   config = resolveConfig()
   if (config == null) return
-
-  window.showInformationMessage('Hello World from iconfont-display!')
-
-  let disposable = commands.registerCommand('iconfontDisplay.start', () => {
-    // window.showInformationMessage('Iconfont-display start!')
-    if (config == null) return
-  })
-
-  context.subscriptions.push(disposable)
 
   registerDecorations(context, config)
   registerCompletions(context, config)
